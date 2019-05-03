9b2e03535e2774e555dd601d62b2343862f1198b
Alan Maldonado <business.alanm@gmail.com>
2019-05-03 09:47:14 -0500

    version-control

--- FS_UI.cpp
+++ FS_UI.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <cstring>
 #include <cmath>
+#define version "Virtual File System 0.1b\nCreated by AEMN & GGM\nWaiting for input...\n"
 
 using namespace std;
 
-int main(){
-	cout << "Virtual File System @ AEMN" << endl;
+int main()
+{
+	cout << version << endl;
+	string command;
+	while (true)
+	{
+		cin >> command;
+	}
 	return 0;
 }
